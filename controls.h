e8a086288e8a1a7592339d9b8cb3f9474bd2fcf0
sustainablelab <sustainablelab@gmail.com>
2022-07-23 01:06:56 -0400

    Roll-back a few X Macros

--- controls.h
+++ controls.h
@@ -5,43 +5,37 @@
 #include <SDL.h>
 
 // Add a control by adding an entry to this table. That's it.
-#define CTRL_TABLE                          \
-    /*    index,label, val, focus, rect, */ \
-        X(R,    "R: ", 128, false, {0})     \
-        X(G,    "G: ", 128, false, {0})     \
-        X(B,    "B: ",   0, false, {0})     \
-        X(A,    "A: ",   0, false, {0})     \
-        X(X,    "X: ",   0, false, {0})     \
-        X(Y,    "Y: ",   0, false, {0})
+#define CTRL_TABLE              \
+    /*    index,label, val  */  \
+        X(  R,  "R: ", 128)     \
+        X(  G,  "G: ", 128)     \
+        X(  B,  "B: ",   0)     \
+        X(  A,  "A: ",   0)     \
+        X(  X,  "X: ",   0)     \
+        X(  Y,  "Y: ",   0)
 
-// Preprocessor output: _Bool ctrl_focus[] = { 0 , 0 , 0 , 0 , };
-#define X(index, label, val, focus, rect) focus,
-bool ctrl_focus[] = { CTRL_TABLE };
+// Preprocessor output:
+// enum controls_index { R, G, B, A, X, Y};
+#define X(index, label, val) index,
+enum controls_index { CTRL_TABLE };
 #undef X
 
-#define NUM_CTRLS (int)(sizeof(ctrl_focus)/sizeof(bool))
-
-// Preprocessor output: enum ctrl_index { R, G, B, A, };
-#define X(index, label, val, focus, rect) index,
-enum ctrl_index { CTRL_TABLE };
+// Preprocessor output:
+// const char *controls_label[] = { "R: ", "G: ", "B: ", "A: ", "X: ", "Y: "};
+#define X(index, label, val) label,
+const char *controls_label[] = { CTRL_TABLE };
 #undef X
 
-// Preprocessor output: const char *ctrl_label[] = { "R: ", "G: ", "B: ", "A: ", };
-#define X(index, label, val, focus, rect) label,
-const char *ctrl_label[] = { CTRL_TABLE };
+// Preprocessor output: int controls_val[] = { 128, 128, 0, 0, 0, 0 };
+#define X(index, label, val) val,
+int controls_val[] = { CTRL_TABLE };
 #undef X
+#define NUM_CTRLS (int)(sizeof(controls_val)/sizeof(int))
 
-// Preprocessor output: int ctrl_val[] = { 128, 128, 0, 0, };
-#define X(index, label, val, focus, rect) val,
-int ctrl_val[] = { CTRL_TABLE };
-#undef X
-
-// Zero-initialize x,y,w,h for the fgnd and bgnd text rectangles
-#define X(index, label, val, focus, rect) rect,
-SDL_Rect ctrl_fg_rect[] = { CTRL_TABLE };
-SDL_Rect ctrl_bg_rect[] = { CTRL_TABLE };
-#undef X
-
+// Declare static mem here; zero-init in ctrl_load_table():
+bool controls_focus[NUM_CTRLS];
+SDL_Rect controls_fg_rect[NUM_CTRLS];
+SDL_Rect controls_bg_rect[NUM_CTRLS];
 
 typedef struct
 {
@@ -54,18 +48,30 @@ typedef struct
     char *buff_c[NUM_CTRLS];                                    // Walk the input buff
     char *buff_end[NUM_CTRLS];                                  // Store where buff ends
     SDL_Texture *tex[NUM_CTRLS];                                // Textures
-    SDL_Rect *fg_rect;                                          // Text bounding box
-    SDL_Rect *bg_rect;                                          // Text box with margins
+    SDL_Rect *fg_rect;                                          // Text inner bounding box
+    SDL_Rect *bg_rect;                                          // Text outer box (margins)
     bool *focus;                                                // Has user's focus cS->focus[i]
 } Ctrl_SOA;
 
 void ctrl_load_table(Ctrl_SOA *cS)
 {
-    cS->label = ctrl_label;
-    cS->val = ctrl_val;
-    cS->focus = ctrl_focus;
-    cS->fg_rect = ctrl_fg_rect;
-    cS->bg_rect = ctrl_bg_rect;
+    cS->label = controls_label;                                 // Initialized by X Macro
+    cS->val = controls_val;                                     // Initialized by X Macro
+    cS->focus = controls_focus;
+    for(int i=0; i<NUM_CTRLS; i++) cS->focus[i] = false;        // Initialize to false
+    cS->fg_rect = controls_fg_rect;
+    for(int i=0; i<NUM_CTRLS; i++)
+    { // Zero-init y,w,h (adjusted during rendering) -- hardcode x to dT_margin
+        cS->fg_rect[i].x = dT_margin;
+        cS->fg_rect[i].y = 0;
+        cS->fg_rect[i].w = 0;
+        cS->fg_rect[i].h = 0;
+    }
+    cS->bg_rect = controls_bg_rect;
+    for(int i=0; i<NUM_CTRLS; i++)
+    { // Zero-init y,w,h (adjusted during rendering) -- hardcode x to 0
+        cS->bg_rect[i] = (SDL_Rect){0};
+    }
 }
 
 void ctrl_has_focus(bool **TheFocus, Ctrl_SOA *cS)
